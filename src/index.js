9093b21b02da4f54f750d87cba201187f2eeed8a
zhuweiyou <i@zhuweiyou.com>
2021-01-21 11:58:44 +0800

    默认不打印 data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const { listen } = require('./bot/ws')
-const sendDogText = require('./handler/send-dog-text')
+const sendDogDiary = require('./handler/send-dog-diary')
 
 listen(data => {
-  console.log(data)
+  // console.log(data)
 
   if (data.message_type === 'group' && data.message === '舔狗日记') {
-    sendDogText(data)
+    sendDogDiary(data)
   }
 })
