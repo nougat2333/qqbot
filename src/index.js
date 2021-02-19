f456bf35db40313b7ac75aa9a2b769de751c5ec6
zhuweiyou <i@zhuweiyou.com>
2021-02-19 10:37:21 +0800

    优化

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-const { listen, send } = require('./bot')
+const bot = require('./bot')
 const dog = require('./service/dog')
 
-listen(async data => {
+bot.listen(async data => {
   console.log(data)
+
+  if (!('message' in data)) {
+    return
+  }
 
   // 他人发送消息带有 "舔狗" 二字
   if (data.message.includes('舔狗')) {
     if (data.message_type === 'group') {
       // 群消息 回复
-      send('send_group_msg', {
+      bot.send('send_group_msg', {
         group_id: data.group_id,
         message: [
           {
@@ -27,7 +31,7 @@ listen(async data => {
       })
     } else if (data.message_type === 'private') {
       // 私聊消息 回复
-      send('send_private_msg', {
+      bot.send('send_private_msg', {
         user_id: data.user_id,
         message: [
           {
